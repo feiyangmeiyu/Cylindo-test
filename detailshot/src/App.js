ebcf212672e5edd019605157d5ad619c30c65610
feiyangmeiyu <806567171@qq.com>
2020-09-24 18:42:15 +0200

    implement drag feature

--- detailshot/src/App.js
+++ detailshot/src/App.js
@@ -6,10 +6,22 @@ import DetailShot from "./components/DetailShot";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { startX: 0, startY: 0, detailHeight: 0, detailWidth: 0 };
+    this.state = {
+      startX: 0,
+      startY: 0,
+      endX: 0,
+      endY: 0,
+      height: 0,
+      width: 0,
+      insideShot: false,
+      dragStartX: 0,
+      dragStartY: 0,
+    };
 
     this.getStartCoor = this.getStartCoor.bind(this);
     this.getEndCoor = this.getEndCoor.bind(this);
+    this.handleDrag = this.handleDrag.bind(this);
+    this.handleInside = this.handleInside.bind(this);
   }
 
   // y1-y2
@@ -25,8 +37,9 @@ class App extends Component {
           <DetailShot
             top={this.state.startY}
             left={this.state.startX}
-            height={this.state.detailHeight}
-            width={this.state.detailWidth}
+            height={this.state.endY - this.state.startY}
+            width={this.state.endX - this.state.startX}
+            handleInside={this.handleInside}
           />
         </div>
       </div>
@@ -35,19 +48,50 @@ class App extends Component {
 
   getStartCoor(e) {
     e.persist();
+    const { startX, startY, endX, endY } = this.state;
 
-    this.setState({ startX: e.clientX, startY: e.clientY });
+    //If the place chosen is not in the detail shot area, then set the start coord again
+    if (
+      (e.clientX < startX || e.clientX > endX) &&
+      (e.clientY < startY || e.clientX > endY)
+    ) {
+      this.setState({ startX: e.clientX, startY: e.clientY });
+    }
   }
 
   getEndCoor(e) {
     e.persist();
+    console.log("father", e.clientX, e.clientY);
+    if (!this.state.insideShot) {
+      const width = e.clientX - this.state.startX;
+      const height = e.clientY - this.state.startY;
 
-    const width = e.clientX - this.state.startX;
-    const height = e.clientY - this.state.startY;
-
-    if (width > 0 && height > 0) {
-      this.setState({ detailWidth: width, detailHeight: height });
+      if (width > 0 && height > 0) {
+        this.setState({ endX: e.clientX, endY: e.clientY });
+      }
+    } else {
+      const dragX = e.clientX - this.state.dragStartX;
+      const dragY = e.clientY - this.state.dragStartY;
+      this.handleDrag(dragX, dragY);
     }
+  }
+
+  handleDrag(dragX, dragY) {
+    this.setState({
+      startX: this.state.startX + dragX,
+      endX: this.state.endX + dragX,
+      startY: this.state.startY + dragY,
+      endY: this.state.endY + dragY,
+      insideShot: false,
+    });
+  }
+
+  handleInside(dragStartX, dragStartY) {
+    this.setState({
+      dragStartX: dragStartX,
+      dragStartY: dragStartY,
+      insideShot: true,
+    });
   }
 }
 
--- detailshot/src/components/DetailShot.js
+++ detailshot/src/components/DetailShot.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class DetailShot extends Component {
   constructor(props) {
     super(props);
+    this.state = { startX: 0, startY: 0, endX: 0, endY: 0 };
+    this.getStartCoor = this.getStartCoor.bind(this);
+    this.getEndCoor = this.getEndCoor.bind(this);
   }
+  componentDidUpdate() {}
 
   render() {
     const { top, left, height, width } = this.props;
@@ -12,8 +16,20 @@ class DetailShot extends Component {
       <div
         className="detail-rec"
         style={{ height: height, width: width, top: top, left: left }}
+        onMouseDown={this.getStartCoor}
+        onMouseUp={this.getEndCoor}
       ></div>
     );
+  }
+
+  getStartCoor(e) {
+    e.stopPropagation();
+    this.setState({ startX: e.clientX, startY: e.clientY });
+    this.props.handleInside(e.clientX, e.clientY);
+  }
+
+  getEndCoor(e) {
+    this.props.handleInside();
   }
 }
 
